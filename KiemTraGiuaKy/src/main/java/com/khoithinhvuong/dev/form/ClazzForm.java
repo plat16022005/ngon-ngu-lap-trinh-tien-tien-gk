8dc09fd7183e3563710ddb267362ae2976e7d86f
DylanVenomania <hoangcamton@gmail.com>
2026-03-11 14:04:51 +0700

    Update service and GUI :CourseForm

---
+++ KiemTraGiuaKy/src/main/java/com/khoithinhvuong/dev/form/ClazzForm.java
@@ -0,0 +1,15 @@
+package com.khoithinhvuong.dev.form;
+
+import javax.swing.*;
+
+public class ClazzForm {
+    private JTable table1;
+    private JTextField txtId;
+    private JTextField txtName;
+    private JTextField txtMaxStudent;
+    private JTextField txtStartDate;
+    private JTextField txtEndDate;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JLabel ID;
+}
